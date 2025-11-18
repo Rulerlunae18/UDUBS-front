9e6247028ee9145e734b3f793f7a1be5fd75453b
Rulerlunae18 <sivenukolga@gmail.com>
2025-11-18 21:32:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [vue()],
+  plugins: [vue()],
+  preview: {
+    allowedHosts: ['udubs-front.onrender.com'],
+    host: true,
+    port: 4173
+  }
 })
